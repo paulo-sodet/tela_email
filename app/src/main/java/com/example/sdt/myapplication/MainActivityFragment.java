9b9070506da6ccd00aaf6c6ac9717095c346126d
pernambucano <jphmf@cin.ufpe.br>
2015-08-07 14:36:25 -0300

    Need Close button and spinner style

--- app/src/main/java/com/example/sdt/myapplication/MainActivityFragment.java
+++ app/src/main/java/com/example/sdt/myapplication/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -18,6 +20,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.sexo, R.layout.spinner_item_actionbar );
+
+        adapter.setDropDownViewResource(R.layout.spinner_item_dropdown);
+
+        spinner.setAdapter(adapter);
+        return rootView;
     }
 }
